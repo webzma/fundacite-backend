e9addff49c081876bd713caf74e261530e389169
Wilberk <ledezma.wilberk@gmail.com>
2025-04-17 23:38:05 -0400

    update CORS origin to localhost for development

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const routes = require("./routes/index");
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://fundacite.vercel.app",
+    origin: "http://localhost:3000",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
